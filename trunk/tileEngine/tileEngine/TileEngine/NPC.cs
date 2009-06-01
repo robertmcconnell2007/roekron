ab0221e58f58541a7e87d09c11b03154eb8d9ad9
IxAtreusAzai <IxAtreusAzai@38067958-41c4-11de-b17b-0148c022b60b>
2009-06-01 09:16:14 +0000

    This is a major update to Game1.cs and tileEngine.cs. It also adds in two class files, MainPlayer.cs and NPC.cs these need to be included into tileEngine/tileEngine/tileEngine. Finally it adds a folder of some basic sprites to be added to the main tileEngine content folder.

---
+++ trunk/tileEngine/tileEngine/TileEngine/NPC.cs
@@ -0,0 +1,26 @@
+﻿#region USINGS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using RolePlayingGameData;
+#endregion
+
+
+namespace RolePlaying
+{
+    public class NPC
+    {
+        public float moveSpeed = 1f;
+        private PlayerPosition npcPosition = new PlayerPosition();
+        public PlayerPosition NPCPosition
+        {
+            get { return NPCPosition; }
+            set { NPCPosition = value; }
+        }
+    }
+}
